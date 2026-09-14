6bcc43ffbefd133c084fbbaadb87e1551f8e27b3
MaxiSmplx <maxischulz2@gmail.com>
2026-09-14 11:13:02 +0200

    Finished Logistic Regression Implementation

--- 2_machine_learning/src/logistic_regression.py
+++ 2_machine_learning/src/logistic_regression.py
@@ -11,8 +11,13 @@ class LogisticRegressionGD():
 
     def sigmoid(self, t):
         return 1 / (1 + np.exp(-t))
+    
+    def log_loss(self, y, y_pred_proba) -> float:
+        boundary = np.finfo(float).eps
+        y_pred_proba = np.clip(y_pred_proba, a_min=boundary, a_max=1-boundary)
+        return -np.mean(y*np.log(y_pred_proba) + (1-y)*np.log(1-y_pred_proba))
 
-    def gradient_descent(self, X, y, learning_rate, max_iter, tol):
+    def gradient_descent(self, X, y, learning_rate, max_iter, tol, verbose):
         n_samples, m_features = X.shape
 
         w, b = np.zeros(m_features), np.float64(0.0)
@@ -20,9 +25,12 @@ class LogisticRegressionGD():
 
         self.X, self.y = X, y
 
-        for _ in range(max_iter):
+        for i in range(max_iter):
+            if verbose and (i+1) % (max_iter//10) == 0:
+                print(f"Iteration {i+1}")
             y_hat = self.sigmoid(X @ w + b)
 
+            # transform w_gradient from normal array into vector of form R^(m x 1)
             w_gradient = np.mean(X * (y_hat - y)[:, np.newaxis], axis=0)
             b_gradient = np.mean(y_hat - y)
 
@@ -34,14 +42,14 @@ class LogisticRegressionGD():
 
         return w, b
 
-    def fit(self, X: pd.DataFrame, y: pd.Series, learning_rate: float = 0.001, max_iter: int = 10_000, tol: float = 1e-6) -> None:
+    def fit(self, X: pd.DataFrame, y: pd.Series, learning_rate: float = 0.001, max_iter: int = 10_000, tol: float = 1e-6, verbose: bool = False) -> None:
         assert type(X) == pd.DataFrame
         assert type(y) == pd.Series
 
         assert X.select_dtypes(include=np.number).shape[1] == X.shape[1]
         assert np.issubdtype(y.dtype, np.number)
 
-        w, b = self.gradient_descent(X, y, learning_rate, max_iter, tol)
+        w, b = self.gradient_descent(X, y, learning_rate, max_iter, tol, verbose)
 
         self.w = w
         self.b = b
@@ -64,6 +72,10 @@ class LogisticRegressionGD():
     @property
     def intercept_(self) -> float:
         return self.b
+    
+    @property
+    def loss_(self) -> float:
+        return self.log_loss(self.y, self.predict_proba(self.X))
 
     @property
     def decision_boundary(self):
